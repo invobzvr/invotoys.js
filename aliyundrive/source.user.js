2237294b3d8fb41dbee45ad4fbb2f399fb855a79
invobzvr <obzvr@outlook.com>
2022-07-06 16:47:17 +0800

    feat: (aliyundrive) update with Box.js

--- aliyundrive/source.user.js
+++ aliyundrive/source.user.js
@@ -2,12 +2,11 @@
 // @name         Custom aliyundrive
 // @name:zh      Custom aliyundrive
 // @namespace    https://github.com/invobzvr
-// @version      1.14
+// @version      1.15
 // @description  阿里云直链导出
 // @author       invobzvr
 // @match        *://www.aliyundrive.com/drive*
 // @match        *://www.aliyundrive.com/s/*
-// @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
@@ -15,6 +14,7 @@
 // @connect      localhost
 // @connect      *
 // @require      https://greasyfork.org/scripts/443030-hook-js/code/Hookjs.js?version=1037826
+// @require      https://greasyfork.org/scripts/447483-box-js/code/Boxjs.js?version=1067657
 // @homepageURL  https://github.com/invobzvr/invotoys.js/tree/main/aliyundrive
 // @supportURL   https://github.com/invobzvr/invotoys.js/issues
 // @license      GPL-3.0
@@ -22,6 +22,7 @@
 
 (function () {
     const that = {
+        Toast: Box.mixin({ toast: true, time: 3e3 }),
         a2config: GM_getValue('a2config', {
             host: '127.0.0.1',
             port: 6800,
@@ -42,112 +43,6 @@
             });
         },
         install: async function () {
-            GM_addStyle(`.that-backdrop {
-    background: #0006;
-    bottom: 0;
-    display: grid;
-    left: 0;
-    overflow: auto;
-    position: fixed;
-    right: 0;
-    top: 0;
-    z-index: 200;
-}
-.that-modal {
-    align-self: center;
-    background: #fff;
-    border-radius: 5px;
-    justify-self: center;
-    margin: 20px;
-    padding: 0 30px;
-    user-select: none;
-}
-.that-title {
-    font-size: 30px;
-    padding: 20px;
-    text-align: center;
-}
-.that-input-group {
-    display: table;
-    margin-bottom: 10px;
-}
-.that-label {
-    display: table-cell;
-    font-size: 16px;
-    padding: 0 10px;
-    text-align: center;
-    width: 100%;
-}
-.that-input {
-    font-size: 20px;
-    padding: 5px 9px;
-}
-.that-input[type=checkbox] {
-    vertical-align: middle;
-}
-.that-options {
-    margin: auto;
-    width: 80%;
-}
-.that-option-item {
-    margin-right: 12px;
-    white-space: nowrap;
-}
-.that-option-item .that-label {
-    font-size: 13px;
-    padding: 0 0 0 3px;
-}
-.that-actions {
-    margin: 20px;
-    text-align: center;
-}
-.that-button {
-    background: #09f;
-    border-radius: 5px;
-    border: none;
-    color: #fff;
-    padding: 7px 18px;
-}
-.that-toastbox {
-    display: grid;
-    min-width: 360px;
-    padding: 10px;
-    position: fixed;
-    right: 0;
-    top: 0;
-    width: 30%;
-    z-index: 201;
-}
-.that-toast-item {
-    background: #09f;
-    border-radius: 7px;
-    box-shadow: 0 2px 10px #0005;
-    color: #fff;
-    margin-bottom: 10px;
-    padding: 10px 10px 15px 10px;
-    opacity: 0;
-    transition: .2s;
-    transform: scale(.8);
-}
-.that-toast-item.in {
-    opacity: 1;
-    transform: scale(1);
-}
-.that-toast-item.success {
-    background: #00a65a;
-}
-.that-toast-item.info {
-    background: #ffa150;
-}
-.that-toast-item.error {
-    background: #dd4b39;
-}
-.that-toast-item .that-title,
-.that-toast-item .that-label {
-    padding: 0 10px;
-    text-align: left;
-    word-break: break-word;
-}`);
             that.inithook();
             addEventListener('pushstate', that.onPushState);
             that.rk = `__reactFiber$${Object.keys(await that.wait('#root', '_reactRootContainer')).find(ii => ii.startsWith('__reactContainer$')).split('$')[1]}`;
@@ -226,10 +121,9 @@
             }
         },
         download: async function (list, func, callback) {
-            list.length !== (list = list.filter(ii => ii.type == 'file')).length && that.toast({
-                type: 'info',
+            list.length !== (list = list.filter(ii => ii.type == 'file')).length && new that.Toast({
+                type: 'warning',
                 title: 'Folders are skipped',
-                timer: 1e3,
             });
             list.length && (await func(list), callback && callback());
         },
@@ -237,33 +131,32 @@
             if (list.length === 1) {
                 location.href = await that.urlOf(list[0]);
             } else {
-                let ctnr = that.modal('that-backdrop', `<div class="that-modal" style="width:50%">
-    <div class="that-title">Urls</div>
-    <textarea style="height:${window.innerHeight * .5}px;width:100%;white-space:nowrap"></textarea>
-    <div class="that-actions">
-        <button class="that-button">OK</button>
-    </div>
-</div>`);
-                ctnr.querySelector('textarea').value = (await Promise.all(list.map(ii => that.urlOf(ii)))).join('\n');
-                ctnr.onclick = evt => ['that-backdrop', 'that-button'].includes(evt.target.className) && ctnr.remove();
+                new Box({
+                    title: 'Urls',
+                    html: `<textarea style="height:${window.innerHeight * .5}px;width:100%;white-space:nowrap"></textarea>`,
+                    didBuild: async modal => {
+                        modal.style.width = '50%';
+                        modal.querySelector('textarea').value = (await Promise.all(list.map(ii => that.urlOf(ii)))).join('\n');
+                    },
+                });
             }
         },
         aria2: async function (list) {
             let a2config;
             if (!that.a2config.remember) {
-                let ret = await that.configa2(false, ctnr => {
+                let ret = await that.configa2(false, modal => {
                     let names = [...document.querySelectorAll('[class*=breadcrumb-item--]')];
                     names = names.slice(1, names.length / 2).map(ii => ii.dataset.label);
-                    ctnr.querySelector('[name=dir]').insertAdjacentHTML('afterend', `<details>
-    <summary><label class="that-option-item"><input class="that-input" name="wds" type="checkbox" onchange="this.closest('details').open = this.checked"><span class="that-label">with directory structure</span></label></summary>
-    <input class="that-input" name="struct" value="${names.join('/')}">
+                    modal.querySelector('[name=dir]').insertAdjacentHTML('afterend', `<details>
+    <summary><label class="box-option-item"><input class="box-input" name="wds" type="checkbox" onchange="this.closest('details').open = this.checked"><span class="box-label">with directory structure</span></label></summary>
+    <input class="box-input" name="struct" value="${names.join('/')}">
 </details>`);
                 });
                 if (ret) {
                     a2config = ret;
                 } else {
-                    return that.toast({
-                        type: 'info',
+                    return new that.Toast({
+                        type: 'warning',
                         title: 'Canceled',
                     });
                 }
@@ -295,9 +188,9 @@
                 url: `http${a2config.https ? 's' : ''}://${a2config.host}:${a2config.port}/jsonrpc`,
                 data: JSON.stringify(data),
             }).catch(err => err);
-            that.toast(res.status == 200 ? {
+            new that.Toast(res.status == 200 ? {
                 type: 'success',
-                title: 'Sent successfully',
+                title: 'Sent to Aria2 successfully',
             } : {
                 type: 'error',
                 title: 'Failed to connect to Aria2',
@@ -307,60 +200,26 @@
         urlOf: async function (model) {
             return model.downloadUrl || model.url || await model.getDownloadUrl();
         },
-        configa2: async function (save, modify) {
-            let ret = await new Promise(res => {
-                let ctnr = that.modal('that-backdrop', `<div class="that-modal">
-    <div class="that-title">Aria2 Config</div>
-    <form>
-        <div class="that-input-group"><span class="that-label">Host</span><input class="that-input" name="host" value="${that.a2config.host}"></div>
-        <div class="that-input-group"><span class="that-label">Port</span><input class="that-input" name="port" value="${that.a2config.port}"></div>
-        <div class="that-input-group"><span class="that-label">Dir</span><input class="that-input" name="dir" value="${that.a2config.dir}"></div>
-        <div class="that-input-group"><span class="that-label">Token</span><input class="that-input" name="token" value="${that.a2config.token || ''}"></div>
-        <div class="that-options">
-            <label class="that-option-item"><input class="that-input" name="https" type="checkbox"${that.a2config.https ? ' checked' : ''}><span class="that-label">Https</span></label>
-            <label class="that-option-item"><input class="that-input" name="remember" type="checkbox"${that.a2config.remember ? ' checked' : ''}><span class="that-label">Remember</span></label>
-        </div>
-    </form>
-    <div class="that-actions">
-        <button class="that-button">OK</button>
+        configa2: async function (save, didBuild) {
+            let ret = await new Box({
+                title: 'Aria2 Config',
+                html: `<form>
+    <div class="box-input-group"><span class="box-label">Host</span><input class="box-input" name="host" value="${that.a2config.host}"></div>
+    <div class="box-input-group"><span class="box-label">Port</span><input class="box-input" name="port" value="${that.a2config.port}"></div>
+    <div class="box-input-group"><span class="box-label">Dir</span><input class="box-input" name="dir" value="${that.a2config.dir}"></div>
+    <div class="box-input-group"><span class="box-label">Token</span><input class="box-input" name="token" value="${that.a2config.token || ''}"></div>
+    <div class="box-options">
+        <label class="box-option-item"><input class="box-input" name="https" type="checkbox"><span class="box-label"${that.a2config.https ? ' checked' : ''}>Https</span></label>
+        <label class="box-option-item"><input class="box-input" name="remember" type="checkbox"><span class="box-label"${that.a2config.remember ? ' checked' : ''}>Remember</span></label>
     </div>
-</div>`);
-                modify && modify(ctnr);
-                ctnr.onclick = evt => {
-                    switch (evt.target.className) {
-                        case 'that-backdrop':
-                            ctnr.remove();
-                            res();
-                            break;
-                        case 'that-button':
-                            ctnr.remove();
-                            res(Object.fromEntries(new FormData(ctnr.querySelector('form'))));
-                            break;
-                    }
-                }
+</form>`,
+                actions: {
+                    OK: modal => Object.fromEntries(new FormData(modal.querySelector('form'))),
+                },
+                didBuild: didBuild,
             });
             ret && (save || ret.remember) && GM_setValue('a2config', that.a2config = ret);
             return ret;
-        },
-        modal: function (ctnrName, innerHTML) {
-            let ctnr = document.querySelector(`.${ctnrName}`);
-            if (!ctnr) {
-                ctnr = document.body.appendChild(document.createElement('div'));
-                ctnr.className = ctnrName;
-            }
-            ctnr[innerHTML instanceof Element ? 'insertAdjacentElement' : 'insertAdjacentHTML']('beforeend', innerHTML);
-            return ctnr;
-        },
-        toast: function (options) {
-            let tst = document.createElement('div'),
-                ctnr = that.modal('that-toastbox', tst);
-            tst.className = `that-toast-item ${options.type || ''}`;
-            tst.innerHTML = `<div class="that-title">${options.title || ''}</div><div class="that-label">${options.text || ''}</div>`;
-            setTimeout(() => tst.classList.add('in'), 10);
-            options.time !== null && setTimeout(() => {
-                tst.classList.remove('in');
-                tst.addEventListener('transitionend', () => (tst.remove(), !ctnr.childElementCount && ctnr.remove()));
-            }, options.time || 3e3);
         },
     };
 
